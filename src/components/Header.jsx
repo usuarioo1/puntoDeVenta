3f7ffb58bf6bcd619d721253f3a8e94298e82dbd
unknown <nicolasgonzalezmoller@gmail.com>
2025-05-13 22:52:09 -0400

    navabr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
     return (
@@ -7,6 +8,39 @@ const Header = () => {
                 <div className='text-2xl font-bold text-gray-800'>
                     Artesanías Pachy
                 </div>
+                <>
+                    <Link href="/bodega/addproduct" passHref legacyBehavior>
+                        <a target="_blank" rel="noopener noreferrer">
+                            <button className="bg-blue-500 text-white px-4 py-2 hover:bg-blue-700 rounded">
+                                Agregar Producto
+                            </button>
+                        </a>
+                    </Link>
+
+                    <Link href="/bodega/editproduct" passHref legacyBehavior>
+                        <a target="_blank" rel="noopener noreferrer">
+                            <button className="bg-yellow-600 text-white px-4 py-2 hover:bg-yellow-800 rounded">
+                                Editar Productos
+                            </button>
+                        </a>
+                    </Link>
+
+                    <Link href="/bodega/ventas" passHref legacyBehavior>
+                        <a target="_blank" rel="noopener noreferrer">
+                            <button className="bg-green-800 text-white px-4 py-2 hover:bg-green-900 rounded">
+                                Ver Ventas
+                            </button>
+                        </a>
+                    </Link>
+
+                    <Link href="/bodega/traslado" passHref legacyBehavior>
+                        <a target="_blank" rel="noopener noreferrer">
+                            <button className="bg-purple-800 text-white px-4 py-2 hover:bg-purple-900 rounded">
+                                Traslado de Productos
+                            </button>
+                        </a>
+                    </Link>
+                </>
             </nav>
         </header>
     )
