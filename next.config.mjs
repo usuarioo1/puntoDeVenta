924b638866b9841caebd76f8de0d277dbb24dcea
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2026-08-29 16:48:59 -0400

    re deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = (phase) => ({
-	distDir: phase === PHASE_DEVELOPMENT_SERVER ? '.next-dev' : '.next-prod',
+	distDir: phase === PHASE_DEVELOPMENT_SERVER ? '.next-dev' : '.next',
 });
 
 export default nextConfig;
